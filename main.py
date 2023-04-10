df352a3507fee9e041a7a32ccfa380ef738f3f84
Onlyizy <kmorelisaac@gmail.com>
2023-04-10 00:06:45 -0400

    added unlock.sh

--- main.py
+++ main.py
@@ -6,19 +6,20 @@ class adb_controller:
     def __init__(self) -> None:
         pass
 
-    def post_wha_story(self):
-        subprocess.run('./post.sh'.split())
+    def unlock(self):
+        subprocess.run('./unlock.sh'.split())
+
+    def post(self):
+        subprocess.run('./post_update.sh'.split())
+    
+    def clean(self):
+        subprocess.run('./clean.sh'.split())
+    
+    def push(self):
+        subprocess.run('./push_content_2_phone.sh'.split())    
     
     def reboot():
         subprocess.run('./reboot.sh'.split())
 
 if __name__ == "__main__":
     adb = adb_controller()
-    fh = file_handler.file_handler()
-    r = fh.push()
-    if not r:
-        with open('log.txt', 'a') as f:
-            f.write(f'{datetime.now()}: Error while pushing files\n')
-    with open('log.txt', 'a') as f:
-        f.write(f'{datetime.now()}: \n')
-    adb.reboot()
---
+++ unlock.sh
@@ -0,0 +1,3 @@
+sleep 5
+echo "unlocking phone"
+adb shell input swipe 392.5 597.9 899.2 899.2 500
