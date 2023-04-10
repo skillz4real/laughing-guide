9398f338c9cf23c0b4b30d2b767f9a701f9e9dc3
Onlyizy <kmorelisaac@gmail.com>
2023-04-09 23:54:02 -0400

    added script to clean and push content to phone

--- capture.sh
+++ capture.sh
@@ -5,7 +5,7 @@ adb shell input tap 266.6 231.8
 
 sleep 5
 echo "taking screenshot"
-adb shell screencap -p /sdcard/DCIM/chat_${DATE}.png
+adb shell screencap -p /sdcard/DCIM/Camera/chat_${DATE}.png
 
 sleep 5
 echo "clicking on status tab"
@@ -17,7 +17,7 @@ adb shell input tap 685 328.7
 
 sleep 5
 echo "taking screenshot"
-adb shell screencap -p /sdcard/DCIM/views_${DATE}.png
+adb shell screencap -p /sdcard/DCIM/Camera/views_${DATE}.png
 
  sleep 5
 echo "clicking on task viewer"
@@ -29,4 +29,4 @@ adb shell input swipe 578.2 1046.1 80.9 1113.0 500
 
 
 #pull screenshot
-cd ./data/ && adb pull /sdcard/DCIM/
+adb pull /sdcard/DCIM/Camera
---
+++ push_content_2_phone.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+echo "pushing files"
+sleep 5
+adb push content/* /sdcard/DCIM/Camera/
