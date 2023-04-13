a738b6640564fe5983a157ad5dc69bc6ec212904
Onlyizy <kmorelisaac@gmail.com>
2023-04-13 03:01:20 -0400

    perfected converter module

--- converter.py
+++ converter.py
@@ -1,12 +1,12 @@
 import moviepy.editor as moviepy
 import os
 
-def to_mp4_converter(dir='/home/ftpuser/whacontent'):
+def to_mp4_converter(dir='/home/ftpuser/whacontent/'):
     content = os.listdir(dir)
     for file in content:
         if str(file).lower().endswith('.mov'):    
-            clip = moviepy.VideoFileClip(f"{dir}/{file}")
-            clip.write_videofile(f"{file.rsplit('.',1)[0]}.mp4")
+            clip = moviepy.VideoFileClip(f"{dir}{file}")
+            clip.write_videofile(f"{dir}{file.rsplit('.',1)[0]}.mp4")
 
 if __name__ == '__main__':
     i = input("would you like to specify a dir (defaults to whacontent in ftpuser's home): ")
