1effee93bdd6b75378bf6ae4f49248fd7d942842
Onlyizy <kmorelisaac@gmail.com>
2023-04-13 11:53:42 -0400

    added log tracking in post.py

--- post.py
+++ post.py
@@ -1,6 +1,9 @@
 from main import adb_controller
+from datetime import datetime
 
 if __name__ == "__main__":
     controller = adb_controller()
     controller.post()
     controller.remove()
+    with open('log.txt', 'a+') as f:
+        f.write(f'{datetime.now()}: New status update posted\n')
