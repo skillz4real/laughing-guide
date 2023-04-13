29a5993454f820ba0d63d62c02e3f9ccf9ba9df5
Onlyizy <kmorelisaac@gmail.com>
2023-04-13 01:05:49 -0400

    added log writer inside file_handler

--- file_handler.py
+++ file_handler.py
@@ -1,9 +1,17 @@
 from main import adb_controller
 from time import sleep
+from datetime import datetime
 
 if __name__=="__main__":
-    controller = adb_controller()
-    controller.push()
-    controller.reboot()
-    sleep(50)
-    controller.unlock()
+    try:
+        controller = adb_controller()
+        controller.push()
+        controller.reboot()
+        sleep(50)
+        controller.unlock()
+        with open('log.txt', 'a+') as f:
+            f.write(f'{datetime.now()}: sucessfully pushed file 2 android rebooted and unlocked phone. Ready 4 action\n')
+
+    except:
+        with open('log.txt', 'a+') as f:
+            f.write(f'{datetime.now()}:Error happened. The dir are probably empty\n')
